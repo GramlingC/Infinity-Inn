a495f9102186a986814c3e3184f6fad9787db517
Kaitlyn Deppmeier <kdeppmeier@yahoo.com>
2017-11-15 20:02:43 -0800

    Added player ghost form
    Starting ghostlike movement mechanics.  Press space to switch between normal and ghost.

---
+++ Assets/_Scripts/IPlayerForms.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPlayerForms
+{
+    //Kind of like a constructor - passes in some parameters from PlayerBehavior to be used by interface members
+    void Init(GameObject player, Rigidbody2D rb, PlayerBehavior pScript);
+
+    //The movement style the player has in given form
+    void PlayerMovement();
+}
+
+public class INormalForm : IPlayerForms
+{
+    GameObject player;
+    PlayerBehavior pScript;
+    Rigidbody2D rb;
+
+    public void Init(GameObject newPlayer, Rigidbody2D newRb, PlayerBehavior newPScript)
+    {
+        player = newPlayer;
+        rb = newRb;
+        pScript = newPScript;
+    }
+
+    public void PlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pScript.SetForm(1);
+            return;
+        }
+        
+        Vector2 moveVelocity = Vector2.zero;
+
+        //Left, right, and jump
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveVelocity += pScript.moveSpeed * Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveVelocity += pScript.moveSpeed * Vector2.right;
+        }
+
+        //Debug.Log(player);
+        moveVelocity.y = rb.velocity.y;
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (Physics2D.Raycast(player.transform.position, Vector2.down, pScript.groundRaycastDist, pScript.groundLayer))
+            {
+                //Raycast towards ground
+                moveVelocity.y += pScript.jumpSpeed;
+            }
+        }
+
+        //Debug.Log(moveVelocity);
+        rb.velocity = moveVelocity;
+    }
+}
+
+public class IGhostForm : IPlayerForms
+{
+    GameObject player;
+    PlayerBehavior pScript;
+    Rigidbody2D rb;
+
+    public void Init(GameObject newPlayer, Rigidbody2D newRb, PlayerBehavior newPScript)
+    {
+        player = newPlayer;
+        rb = newRb;
+        pScript = newPScript;
+    }
+
+    public void PlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pScript.SetForm(0);
+            return;
+        }
+        Vector2 moveVelocity = Vector2.zero;
+
+        //Left, right, and jump
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveVelocity += pScript.moveSpeed * Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveVelocity += pScript.moveSpeed * Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            moveVelocity += pScript.moveSpeed * Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            moveVelocity += pScript.moveSpeed * Vector2.down;
+        }
+
+        //Debug.Log(moveVelocity);
+        rb.velocity = moveVelocity;
+    }
+}
--- Assets/_Scripts/PlayerBehavior.cs
+++ Assets/_Scripts/PlayerBehavior.cs
@@ -12,16 +12,25 @@ public class PlayerBehavior : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private INormalForm normalForm = new INormalForm();
+    private IGhostForm ghostForm = new IGhostForm();
+
+    private IPlayerForms currForm;
+
 	// Use this for initialization
 	void Start ()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();	
-	}
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        currForm = normalForm;
+
+        normalForm.Init(gameObject, rb, this);
+        ghostForm.Init(gameObject, rb, this);        
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Vector2 moveVelocity = Vector2.zero;
+        /*Vector2 moveVelocity = Vector2.zero;
 
         //Left, right, and jump
 	    if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
@@ -45,14 +54,37 @@ public class PlayerBehavior : MonoBehaviour
         }
 
         //Debug.Log(moveVelocity);
-        rb.velocity = moveVelocity;
+        rb.velocity = moveVelocity;*/
+        currForm.PlayerMovement();
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        //May depend on what form the player is in
         if (other.gameObject.tag.Equals("Lethal"))
         {
             Debug.Log("Dead");
+        }
+    }
+
+    //Method that sets certain variables and components of player based on the player's form
+    public void SetForm(int formNum)
+    {
+        //0 = normal form
+        //1 = ghost form
+        switch (formNum)
+        {
+            case 0:
+                gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                rb.gravityScale = 1;
+                currForm = normalForm;
+                break;
+            case 1:
+                gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                rb.gravityScale = 0;
+                rb.velocity = Vector2.zero;
+                currForm = ghostForm;
+                break;
         }
     }
 }
